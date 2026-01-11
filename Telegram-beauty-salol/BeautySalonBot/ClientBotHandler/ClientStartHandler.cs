4b990dde261fd8259ea41daa7267b82661bee8be
yuliya <@foxstudent107445>
2026-01-11 23:25:43 +0900

    refactor: edit messeges

--- Telegram-beauty-salol/BeautySalonBot/ClientBotHandler/ClientStartHandler.cs
+++ Telegram-beauty-salol/BeautySalonBot/ClientBotHandler/ClientStartHandler.cs
@@ -9,6 +9,7 @@ namespace BeautySalonBot.ClientBotHandler;
 public class ClientStartHandler : IUpdateHandler
 {
     private readonly IProcedureRepository _procedureRepository;
+    private static readonly Dictionary<long, string> _waitingForComment = new();
 
     public ClientStartHandler(IProcedureRepository procedureRepository)
     {
@@ -31,11 +32,55 @@ public class ClientStartHandler : IUpdateHandler
         if (update.Type == UpdateType.Message && update.Message?.Text == "/start")
         {
             await HandleStartCommand(botClient, update, ct);
+            return;
         }
-        else if ( update.Type == UpdateType.CallbackQuery)
+
+        if (update.Type == UpdateType.Message &&
+            update.Message?.Text != null &&
+            _waitingForComment.TryGetValue(update.Message.From.Id, out var procedureName))
         {
-            await HandleCallbackQueryAsync(botClient, update.CallbackQuery, ct);
+            var comment = update.Message.Text;
+            var chatId = update.Message.Chat.Id;
+
+            _waitingForComment.Remove(update.Message.From.Id);
+
+            var now = DateTime.UtcNow.AddHours(9);
+            var time = now.TimeOfDay;
+
+            // Ответ пользователю
+            await botClient.SendMessage(
+                chatId,
+                $"{update.Message.From.FirstName}, спасибо! 🤍\n" +
+                "Ваша заявка принята.\n" +
+                "Наш администратор свяжется с вами в ближайшее время ✨",
+                cancellationToken: ct);
+
+            // Сообщение админу
+            await botClient.SendMessage(
+                chatId: -5031976519,
+                text: $"📥 Запрос на запись:\n" +
+                      $"👤 Клиент: [{update.Message.From.FirstName}](tg://user?id={update.Message.From.Id})\n" +
+                      $"💅 Процедура: {procedureName}\n" +
+                      $"💬 Комментарий: {comment}\n\n" +
+                      $"Сообщение было отправлено в: {time}",
+                parseMode: ParseMode.Markdown,
+                replyMarkup: new InlineKeyboardMarkup(new[]
+                {
+                    new[]
+                    {
+                        InlineKeyboardButton.WithCallbackData("✅ Подтвержден", $"reaction:called"),
+                        InlineKeyboardButton.WithCallbackData("🔁 Перезвонить", "reaction:retry")
+                    },
+                    new[]
+                    {
+                        InlineKeyboardButton.WithCallbackData("❌ Не дозвонились", "reaction:missed"),
+                        InlineKeyboardButton.WithCallbackData("🚫 Неинтересно", "reaction:ignored")
+                    }
+                }),
+                cancellationToken: ct);
             
+
+            return;
         }
     }
 
@@ -55,11 +100,10 @@ public class ClientStartHandler : IUpdateHandler
 
         await botClient.SendMessage(
             chatId,
-            $"Здравствуйте, {firstName}! 👋\n\n" +
-            $"Вас приветствует виртуальный помощник.\n" +
-            $"Для того чтобы отправить заявку, выберите процедуру из списка ниже\n" +
-            $"Чтобы выбрать вторую процедуру нажмите на другую процедуру из списка ниже\n" +
-            $"Спасибо что выбрали нас✨✨✨",
+            $"Здравствуйте, {firstName}!✨\n" +
+            $"Спасибо за обращение в BEAUTY ZONE!✨\n\n" +
+            $"Вас приветствует виртуальный помощник студии красоты Beauty Zone.\n" +
+            $"Пожалуйста, выберите желаемую процедуру из списка ниже 🤍",
             replyMarkup: inlineKeyboard,
             cancellationToken: ct);
     }
@@ -71,80 +115,28 @@ public class ClientStartHandler : IUpdateHandler
         
         var now = DateTime.UtcNow.AddHours(9);
         var time = now.TimeOfDay;
-        var startOfDay = new TimeSpan(9, 0, 0);
-        var endOfDay   = new TimeSpan(21, 0, 0); 
+        var comment = string.Empty;
 
+        
         if (data.StartsWith("select_procedure:"))
         {
             var procedureName = data.Split(':')[1];
+            _waitingForComment[query.From.Id] = procedureName;
 
             await bot.AnswerCallbackQuery(
                 query.Id,
                 $"Вы выбрали процедуру: {procedureName}"
             );
-            if (time < startOfDay || time >= endOfDay)
-            {
-                await bot.SendMessage(
-                    chatId,
-                    $"🌙 Сейчас мы вне рабочего времени.\n" +
-                    $"Наши часы работы: с 09:00 до 21:00.\n\n" +
-                    $"В ближайший рабочий промежуток с вами свяжется наш менеджер\n" +
-                    $"чтобы подтвердить запись и помочь с выбором удобного времени.💖\n" +
-                    $"Спасибо что выбрали нас✨✨✨",
-                    cancellationToken: ct);
-
-                await bot.SendMessage(
-                    chatId: -5031976519,
-                    text: $"📥 Запрос на запись:\n" +
-                          $"👤 Клиент: [{query.From.FirstName }](tg://user?id={query.From.Id})\n" +
-                          $"💅 Процедура: {procedureName}\n\n",
-                    parseMode: ParseMode.Markdown,
-                    replyMarkup: new InlineKeyboardMarkup(new[]
-                    {
-                        new[]
-                        {
-                            InlineKeyboardButton.WithCallbackData("✅ Подтвержден", $"reaction:called"),
-                            InlineKeyboardButton.WithCallbackData("🔁 Перезвонить", "reaction:retry")
-                        },
-                        new[]
-                        {
-                            InlineKeyboardButton.WithCallbackData("❌ Не дозвонились", "reaction:missed"),
-                            InlineKeyboardButton.WithCallbackData("🚫 Неинтересно", "reaction:ignored")
-                        }
-                    }),
-                    cancellationToken: ct
-                );
-            }
 
             await bot.SendMessage(
                 chatId,
-                $"Спасибо! Вы выбрали: *{procedureName}* 😊" +
-                $" В ближайшее время с вами свяжется наш администратор для подтверждения записи. Хорошего дня!",
-                parseMode: ParseMode.Markdown,
+                $"{query.From.FirstName}, благодарим за выбор 🤍\n" +
+                "Отправьте, пожалуйста, сообщение с желаемой датой и временем для записи 🤍\n" +
+                "Мы проверим доступность и подтвердим ✨",
                 cancellationToken: ct
             );
-            await bot.SendMessage(
-                chatId: -5031976519,
-                text: $"📥 Запрос на запись:\n" +
-                      $"👤 Клиент: [{query.From.FirstName }](tg://user?id={query.From.Id})\n" +
-                      $"💅 Процедура: {procedureName}\n\n",
-                parseMode: ParseMode.Markdown,
-                replyMarkup: new InlineKeyboardMarkup(new[]
-                {
-                    new[]
-                    {
-                        InlineKeyboardButton.WithCallbackData("✅ Подтвержден", $"reaction:called"),
-                        InlineKeyboardButton.WithCallbackData("🔁 Перезвонить", "reaction:retry")
-                    },
-                    new[]
-                    {
-                        InlineKeyboardButton.WithCallbackData("❌ Не дозвонились", "reaction:missed"),
-                        InlineKeyboardButton.WithCallbackData("🚫 Неинтересно", "reaction:ignored")
-                    }
-                }),
-                cancellationToken: ct
-            );
-           
+
+            return;
         }
         if (data.StartsWith("reaction:"))
         {
